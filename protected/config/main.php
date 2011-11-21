d521c155bac5e344465a635421c478fe43d79ce2
ATI3D <soccer007@mail.ru>
2011-11-21 22:48:39 +0400

    edit

--- protected/config/main.php
+++ protected/config/main.php
@@ -26,7 +26,7 @@ return array(
 		// uncomment the following to enable the Gii tool
 		'gii'=>array(
 			'class'=>'system.gii.GiiModule',
-			'password'=>'tested',
+			'password'=>'password',
 		 	// If removed, Gii defaults to localhost only. Edit carefully to taste.
 			'ipFilters'=>array('127.0.0.1','::1'),
 		),
