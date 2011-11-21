8e5a46b6ed096048722cf0cbcec05076db0280f7
ATI3D <soccer007@mail.ru>
2011-11-21 23:11:06 +0400

    ATI3D home

--- protected/config/main.php
+++ protected/config/main.php
@@ -61,7 +61,7 @@ return array(
 		),
 		// uncomment the following to use a MySQL database
 		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=game',
+			'connectionString' => 'mysql:host=localhost;dbname=blog',
 			'emulatePrepare' => true,
 			'username' => 'ati',
 			'password' => 'tested',
