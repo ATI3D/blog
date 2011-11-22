0054b150751b84412070e1eaac3a0437598d79d8
ATI3D <soccer007@mail.ru>
2011-11-22 23:57:01 +0400

    update project

--- protected/views/post/update.php
+++ protected/views/post/update.php
@@ -1,18 +1,18 @@
 <?php
 $this->breadcrumbs=array(
-	'Posts'=>array('index'),
+	'Записи'=>array('index'),
 	$model->title=>array('view','id'=>$model->id),
-	'Update',
+	'Изменение',
 );
 
 $this->menu=array(
-	array('label'=>'List Post', 'url'=>array('index')),
-	array('label'=>'Create Post', 'url'=>array('create')),
-	array('label'=>'View Post', 'url'=>array('view', 'id'=>$model->id)),
-	array('label'=>'Manage Post', 'url'=>array('admin')),
+	//array('label'=>'List Post', 'url'=>array('index')),
+	array('label'=>'Создать', 'url'=>array('create')),
+	//array('label'=>'View Post', 'url'=>array('view', 'id'=>$model->id)),
+	array('label'=>'Управление', 'url'=>array('admin')),
 );
 ?>
 
-<h1>Update Post <?php echo $model->id; ?></h1>
+<h1>Изменение записи &rarr; "<?php echo $model->title; ?>"</h1>
 
 <?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
